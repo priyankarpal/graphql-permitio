3060121636e2f899b76455f9220a539c2be8c739
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2024-07-01 19:17:32 +0530

    server is running

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import Permit from 'permitio';
+import taskResolvers from './resolvers/taskResolvers.js';
+import typeDefs from './schema.js';
+
+const { Permit: PermitClass }=Permit;
+
+const permit=new PermitClass({
+    token: process.env.PERMIT_API_KEY,
+});
+
+const server=new ApolloServer({
+    typeDefs,
+    resolvers: taskResolvers,
+    context: async ({ req }) => {
+        const user=await permit.getUserFromRequest(req);
+        return { user, permit };
+    },
+});
+
+startStandaloneServer(server, {
+    listen: { port: 4000 },
+}).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
